50fc806abc8b66075b8798386b70aa934b8c09a6
Mohnish Kalia <mohnish.kalia@gmail.com>
2023-02-09 02:42:18 -0600

    Add imagemin for now, remove compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,35 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import viteImagemin from 'vite-plugin-imagemin'
 import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), viteCompression({
-    filter: /\.(cdb|png|jpg|wasm|js|mjs|json|css|html)$/i,
-  })],
+  plugins: [
+    svelte(),
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      mozjpeg: {
+        quality: 20,
+      },
+      pngquant: {
+        quality: [0.5, 0.6],
+        speed: 4,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
 })
