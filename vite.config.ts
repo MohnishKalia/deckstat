ce764c6fdabac273f76378ef74877920ca3885ae
Mohnish Kalia <mohnish.kalia@gmail.com>
2023-02-09 01:40:27 -0600

    Add compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), viteCompression({
+    filter: /\.(cdb|png|jpg|wasm|js|mjs|json|css|html)$/i,
+  })],
 })
